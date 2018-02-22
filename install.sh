cfb9b54af5ce31a1d366205298f6e200e09c9366
Eric_Office <yuema@stanford.edu>
2018-02-21 20:47:12 -0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.vim/colors
 ln -sf ~/dotfiles/flattened_dark.vim ~/.vim/colors/flattened_dark.vim
 ln -sf ~/dotfiles/flattened_light.vim ~/.vim/colors/flattened_light.vim
+ln -sf ~/dotfiles/solarized.vim ~/.vim/colors/solarized.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 # bash
