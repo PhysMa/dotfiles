7c475e34b62be65da4c028b93eba73c7bedb9757
Eric_Office <yuema@stanford.edu>
2018-02-21 17:58:49 -0800

    Added vim files.

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 # tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# vim
+mkdir -p ~/.vim/colors
+ln -sf ~/dotfiles/flattened_dark.vim ~/.vim/colors/flattened_dark.vim
+ln -sf ~/dotfiles/flattened_light.vim ~/.vim/colors/flattened_light.vim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+# bash
